75425d5d370172d54d4eac1d8931b747b66821b6
mzgcz <j.wenjiao@gmail.com>
2014-05-30 16:28:19 +0800

    改善100 doors

--- 100_doors.lisp
+++ 100_doors.lisp
@@ -1,24 +1,25 @@
 ;; http://rosettacode.org/wiki/100_doors
 
 ;;; 每次访问时，变更门的状态
-(defun change-door (door)
+(defun switch-door (door)
   (not door))
 
 ;;; 第no次访问时，访问no的倍数号门
 (defun access-door (no N doors)
-  (loop for door in doors
-     for i from 1 to N collect
-       (if (= 0 (second (multiple-value-list (floor i no))))
-           (change-door door)
-           door)))
+  (let ((copy-doors (copy-list doors)))
+    (loop for i from no upto N by no
+       do (setf (nth (1- i) copy-doors) (switch-door (nth (1- i) copy-doors))))
+    copy-doors))
 
 ;;; 启动
 (defun start-doors-game (&optional (N 100))
   ;;; N扇门，初始状态为关闭
-  (let ((doors (make-list N :initial-element nil)))
+  (let ((doors (make-list N :initial-element nil))
+        (no-list (loop for i from 1 upto 100 collect i)))
     ;;; 访问N次
-    (loop for i from 1 to N
-       do (setf doors (access-door i N doors)))
+    (mapcar (lambda (no)
+              (setf doors (access-door no N doors)))
+            no-list)
     doors))
 
 (print (start-doors-game))
